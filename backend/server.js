9bee4803320fa4ac9b9dd1e5ce7e0ec533817bf6
vedant115 <vedant.a.rahane@gmail.com>
2025-06-29 20:12:03 +0530

    fix cors issue for depoyment

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,14 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173", // Frontend URL
-    credentials: true, // Allow cookies
+    origin:
+      process.env.NODE_ENV === "production"
+        ? [
+            "https://careernest-frontend.onrender.com",
+            "https://careernest-frontend-skyh.onrender.com",
+          ]
+        : "http://localhost:5173",
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
