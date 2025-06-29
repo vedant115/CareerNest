090039673fcafd8a6959a50ac5d4859595818d69
vedant115 <vedant.a.rahane@gmail.com>
2025-06-29 19:58:11 +0530

    update vite.config.js to allow render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dns from "dns";
+
+// This helps with localhost resolution issues
+dns.setDefaultResultOrder("verbatim");
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: "0.0.0.0",
+    port: 10002,
+    strictPort: true,
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      ".onrender.com",
+      "careernest-frontend.onrender.com",
+    ],
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: false,
+    cors: true,
+  },
+});
